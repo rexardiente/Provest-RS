965dcb693a63d0246732d9223f49cade8e23402e
Rex C. Ardiente <rcadiente@vauldex.com>
2018-06-11 15:39:56 +0800

    Add - Other Modules
    Add Riot JS and other Configurations.

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import javax.inject.{ Inject, Singleton }
+import javax.inject._
 import scala.concurrent.Future
 import scala.concurrent.ExecutionContext
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,35 +13,37 @@ import play.api.mvc.WebSocket._
 import play.api.data.Forms._
 import play.api.libs.json._
 import play.api.libs.streams._
-import play.api.libs.openid._
-import actors.ClientManagerActor
+import play.api.i18n.{ I18nSupport, MessagesApi }
+import actors._
+
+import ejisan.play.libs.{ PageMetaSupport, PageMetaApi }
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject()(
-    implicit system: ActorSystem,
-    openIdClient: OpenIdClient,
-    c: ControllerComponents,
-    implicit val ec: ExecutionContext,
-    implicit val materializer: Materializer,
-    implicit val configuration: play.api.Configuration
-  ) extends AbstractController(c) {
+class HomeController @Inject() (
+  implicit val system: ActorSystem,
+  implicit val materializer: Materializer,
+  val messagesApi: MessagesApi,
+  val pageMetaApi: PageMetaApi,
+  implicit val wja: WebJarAssets
+) extends Controller with I18nSupport with PageMetaSupport {
   implicit val messageFlowTransformer = MessageFlowTransformer.jsonMessageFlowTransformer[JsValue, JsValue]
   /**
-   * Create an Action to render an HTML page with a welcome message.
+   * Create an Action to render an HTML page.
    * The configuration in the `routes` file means that this method
    * will be called when the application receives a `GET` request with
    * a path of `/`.
-  */
+   */
 
   def ws = WebSocket.accept[JsValue, JsValue] { implicit request =>
     ActorFlow.actorRef(out => ClientManagerActor.props(out))
   }
 
-  def login = Action.async { request =>
-    Future.successful(Ok(""))
+  def index = Action { implicit request =>
+    println("Testing")
+    Ok(views.html.page())
   }
 }
